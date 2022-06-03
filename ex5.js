3c484517ce3b886e90b9a60b3a9a4741b1233ad8
fataranto <fataranto@gmail.com>
2022-06-03 20:17:47 +0200

    4 ok

--- ex5.js
+++ ex5.js
@@ -4,9 +4,10 @@ const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present',
 
 // Usa el método filter para filtrar las palabras que empiezan por la letra 'e'
 
-console.log(words.filter()); // TODO [ 'elite', 'exuberant']
+console.log(words.filter(v => v[0]=="e")); // TODO [ 'elite', 'exuberant']
+
 
 // Usa el método filter para filtrar las palabras que empiezan por vocal
 // BONUS: Usa una expresión regular!
 
-console.log(words.filter()); // TODO [ 'elite', 'exuberant', 'away']
+console.log(words.filter(v => v[0].match(/[aeiou]/gi))); // TODO [ 'elite', 'exuberant', 'away']
