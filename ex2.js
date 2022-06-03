082c73284d07358514628c4142b959fe42fead1a
fataranto <fataranto@gmail.com>
2022-06-03 19:25:35 +0200

    2 ok

--- ex2.js
+++ ex2.js
@@ -4,6 +4,10 @@
 
 let colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 
-let resultado = colors.map()
+let resultado = colors.map(valPos);
+
+function valPos (v,i){
+    return `${i}-${v}`
+}
 
 console.log(resultado); // ['0-Blue', '1-Green', '2-Red', '3-Orange', '4-Violet', '5-Indigo', '6-Yellow']
